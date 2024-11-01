f775a63a8ffb89218ad1db8227d3e04db576ce9d
conokisekai <kambiconrad@gmail.com>
2024-11-01 17:25:33 +0300

    Add basic HTML on App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,47 +1,50 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+  import { onMount } from "svelte";
+  let city = "New York";
+  let searchQuery = "";
+  let temperature = "22°C";
+  let weatherIcon = "/images/rain.png";
+  let humidity = "50%";
+  let windSpeed = "15 km/h";
+
+  const handleSearch = (event) => {
+    event.preventDefault(); 
+    if (searchQuery.trim()) {
+      city = searchQuery;
+      searchQuery = "";
+    }
+  };
 </script>
 
-<main>
-  <div>
-    <a href="https://vite.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
+<div class="card">
+  <form class="search" on:submit={handleSearch}>
+    <input
+      type="text"
+      placeholder="Enter city name"
+      spellcheck="false"
+      bind:value={searchQuery}
+    />
+    <button type="submit"><img src="/images/search.png" alt="Search" /></button>
+  </form>
+  <div class="weather">
+    <img src={weatherIcon} class="weather-icon" alt="Weather Icon" />
+    <h1 class="temp">{temperature}</h1>
+    <h2 class="city">{city}</h2>
+    <div class="details">
+      <div class="col">
+        <img src="/images/humidity.png" alt="Humidity" />
+        <p class="humidity">{humidity}</p>
+        <p>Humidity</p>
+      </div>
+      <div class="col">
+        <img src="/images/wind.png" alt="Wind Speed" />
+        <p class="wind">{windSpeed}</p>
+        <p>Wind Speed</p>
+      </div>
+    </div>
   </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
-</main>
+</div>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
+  
 </style>
